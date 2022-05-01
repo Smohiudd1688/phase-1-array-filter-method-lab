b49028f0db7d4c75617be8bcfe95cd1431b3a3d9
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-05-01 01:24:13 -0700

    commit tree 2a9a4572d4c5d89999792a00d074b3cc4c557894

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+function findMatching(driversArray, driverName) {
+    return driversArray.filter(function (name) {
+        return (driverName.toUpperCase() === name.toUpperCase());
+    });
+}
+
+function fuzzyMatch(driversArray, letters) {
+    const areLettersEqual = name => {
+        const size = letters.length;
+        const tempName = name.slice(0, size);
+        return (letters === tempName);
+    };
+
+    return driversArray.filter(areLettersEqual);
+}
+
+function matchName (driversArray, driverName) {
+    function isNameEqual (driver) {
+        if (driver.name === driverName) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return driversArray.filter(isNameEqual);
+}
